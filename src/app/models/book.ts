f0b21ffffb6323c34577a0186dd0b735632496d0
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2023-12-17 12:11:37 +0100

    books

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -7,10 +7,7 @@ export class Book {
         public price: number,
         public photo: string,
         public id_books?:number,
-        public id_user?: number ){
-
-        this.id_books = 0;
-        this.id_user = 0;    
+        public id_user?: number ){   
 
         }  
 }
--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
+
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
 
+  books: Book[];
+
+  constructor(){
+
+    this.books = [
+      new Book("El día que Nietzsche lloró", "blanda", "Irvin D. Yalom", 19, "/assets/img/imgBooks/Nietzsche.jpeg", 111 ),
+      new Book("El monje que vendió su ferrari", "blanda", "Robin Sharma", 11, "/assets/img/imgBooks/monje.jpg", 112),
+      new Book("Reina roja", "dura ", "Juan Gómez-Jurado", 21,"/assets/img/imgBooks/reina.jpg", 113 ),
+
+    ];
+  }
+
+
+  ngOnInit(): void{
+  }
 }
