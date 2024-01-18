ef9107321bcb75c446e629b1c15c419b5c3ceb08
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-01-18 19:47:15 +0100

    add-books

--- src/app/models/response.ts
+++ src/app/models/response.ts
@@ -3,6 +3,6 @@ import { Book } from "./book";
 export class Response {
     constructor(public err: boolean,
                 public code: number, 
-                public missage: string, 
-                public data: Book){}
+                public message: string, 
+                public data: Book[]){}
 }
